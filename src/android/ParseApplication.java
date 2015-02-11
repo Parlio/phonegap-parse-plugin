3fdf95852e01f2b548a82efb1612a45018c74a5e
Osman <osman@beebly.com>
2015-02-10 16:06:37 -0800

    Adding proper strings

--- src/android/ParseApplication.java
+++ src/android/ParseApplication.java
@@ -24,7 +24,7 @@ public class ParseApplication extends Application
     @Override
     public void onCreate() {
         super.onCreate();
-        Parse.initialize(this, "YNgix1K8f1vkGBUxqgkhzU7AlotSX0Rc01pQmfvy", "wSS5xNditxV9fTng7kLUmchKrTLkMgnjb7vUThyH");
+        Parse.initialize(this, "PARSE_APP_ID", "PARSE_CLIENT_KEY");
         PushService.setDefaultPushCallback(this, CordovaApp.class);
         ParseInstallation.getCurrentInstallation().saveInBackground();
     }
